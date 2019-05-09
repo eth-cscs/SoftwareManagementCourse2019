86b6413660a765bf0aa87121952608e5ec6106c2
Hannes Vogt <hannes@havogt.de>
2019-05-09 16:23:45 +0200

    updated FindDotprod.cmake

--- cmake/handson/2_modern_cmake/excercise/dotprodcl/cmake/FindDotprod.cmake
+++ cmake/handson/2_modern_cmake/excercise/dotprodcl/cmake/FindDotprod.cmake
@@ -9,7 +9,7 @@ Imported targets
 
 This module defines the following :prop_tgt:`IMPORTED` targets:
 
-``dotprod::Dotprod``
+``Dotprod::Dotprod``
 The dotprod library, if found.
 
 Result variables
@@ -20,47 +20,58 @@ This module will set the following variables in your project:
 ``Dotprod_FOUND``
 true if the dotprod headers and libraries were found
 
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``Dotprod_INCLUDE_DIR``
+  The directory containing ``foo.h``.
+``Dotprod_LIBRARY``
+  The path to the Foo library.
 
 #]=======================================================================]
 
-if(NOT(DEFINED DOTPROD_ROOT))
-    find_path(DOTPROD_ROOT NAMES include/dotprod/dotprod.h
+if(NOT(DEFINED Dotprod_ROOT_DIR))
+    find_path(Dotprod_ROOT_DIR NAMES include/dotprod/dotprod.h
+        DOC "Path to the dotprod install directory"
         PATHS
             ${Dotprod_DIR}
+            ${DOTPROD_ROOT}
             $ENV{Dotprod_DIR}
             $ENV{DOTPROD_ROOT}
         )
 endif()
 
-find_library(_dotprod_lib
+find_library(Dotprod_LIBRARY
     NAMES dotprod
     HINTS
-        ${DOTPROD_ROOT}/lib64
-        ${DOTPROD_ROOT}/lib
+        ${Dotprod_ROOT_DIR}/lib64
+        ${Dotprod_ROOT_DIR}/lib
     )
+mark_as_advanced(Dotprod_LIBRARY)
 
-find_path(_dotprod_include_dir
+find_path(Dotprod_INCLUDE_DIR
     NAMES dotprod/dotprod.h
-    HINTS ${DOTPROD_ROOT}/include
+    HINTS ${Dotprod_ROOT_DIR}/include
     )
+mark_as_advanced(Dotprod_INCLUDE_DIR)
 
-if(_dotprod_lib)
-    if(NOT TARGET dotprod::Dotprod)
+if(Dotprod_LIBRARY)
+    if(NOT TARGET Dotprod::Dotprod)
         if(NOT Dotprod_FIND_QUIETLY)
             message(STATUS "Found Dotprod!")
         endif()
-        add_library(dotprod::Dotprod INTERFACE IMPORTED)
-        target_include_directories(dotprod::Dotprod INTERFACE ${_dotprod_include_dir})
-        target_link_libraries(dotprod::Dotprod INTERFACE ${_dotprod_lib})
+        add_library(Dotprod::Dotprod INTERFACE IMPORTED)
+        target_include_directories(Dotprod::Dotprod INTERFACE ${Dotprod_INCLUDE_DIR})
+        target_link_libraries(Dotprod::Dotprod INTERFACE ${Dotprod_LIBRARY})
         set(Dotprod_FOUND TRUE)
     endif()
 else()
     if(Dotprod_FIND_REQUIRED)
         message(FATAL_ERROR "Required library Dotprod was not found!")
-    else()
-        if(NOT Dotprod_FIND_QUIETLY)
-            message("Library Dotprod was not found!")
-        endif()
+    elseif(NOT Dotprod_FIND_QUIETLY)
+        message("Library Dotprod was not found!")
     endif()
     unset(Dotprod_FOUND)
 endif()
