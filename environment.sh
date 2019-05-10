2f809526b2e7a1bd345f0e78cc1016cf8348bb34
Hannes Vogt <hannes@havogt.de>
2019-05-10 10:04:18 +0200

    env

---
+++ environment.sh
@@ -0,0 +1,8 @@
+# Environment for the course
+
+module load daint-gpu
+module switch PrgEnv-cray PrgEnv-gnu
+module load cudatoolkit/9.2.148_3.19-6.0.7.1_2.1__g3d9acc8
+module load /users/vogtha/easybuild/daint/haswell/modules/all/CMake/3.14.1
+module load Boost
+
