f3821f84577975509d3d377996a7dfb8b6b01626
John Biddiscombe <biddisco@cscs.ch>
2019-05-12 14:15:37 +0200

    Make a complete script for x-file test

--- git/test_1.sh
+++ git/test_1.sh
@@ -1,2 +1,11 @@
+mkdir git_test
+cd git_test
+git init .
+du -ch .
 for l in {1..8192} ; do printf 'x%.0s' {1..128}; printf "\n"; done > x.txt
-
+git st
+git add x.txt
+git st
+git commit -m"New file containing only x's"
+git log
+du -ch .
